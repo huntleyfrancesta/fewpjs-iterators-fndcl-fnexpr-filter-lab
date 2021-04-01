ba64b5074c0fe7e3705895e38d86e17f3c58b70e
francesta <huntleyfrancesta@gmail.com>
2021-04-01 17:46:47 -0400

    commit tree 54dcb0add1b4e29974cd29dfd5e24eb6a3e739df

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+
+function findMatching(drivers, string) {
+    let result = drivers.filter(name => name.toUpperCase() === string.toUpperCase());
+    return result;
+}
+
+drivers.filter(name => name === "Bobby")
+
+// str.substring(1,3); 
+// str.indexOf
+
+function fuzzyMatch(drivers, substring) {
+    return drivers.filter(match => match.toUpperCase().indexOf(substring.toUpperCase()) === 0)
+}
+
+
+//const drivers = [{name:'Bobby'}, {name:'Sammy'}, {name:'Sally'}, {name:'Annette'}, {name:'Sarah'}, {name:'Bobby'}];
+
+function matchName(drivers, string) {
+    return drivers.filter(match => match["name"] === string)
+}
+
+
+// objects; using filter
